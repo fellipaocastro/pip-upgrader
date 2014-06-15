2af15051dac7ec0936af3f0c1bbd9d7177355c26
Fellipe Castro <contact@fellipecastro.com>
2014-06-15 18:31:29 -0300

    Update repository

--- pip-upgrader.sh
+++ pip-upgrader.sh
@@ -1,11 +1,18 @@
-pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip install -U
+VIRTUALENV=$(python -c 'import sys; print sys.real_prefix' 2>/dev/null && INVENV=1 || INVENV=0)
+# VIRTUALENV=`python -c 'import sys; print sys.real_prefix' 2>/dev/null && INVENV=1 || INVENV=0`
 
-echo 'Do you want to save a new requirements.txt?'
-select yn in 'Yes' 'No'; do
-    case $yn in
-        Yes)
-            pip freeze --local > "$PWD/requirements.txt"; break;;
-        No)
-            exit;;
-    esac
-done
+if [ "$VIRTUALENV" != '' ]; then
+    pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip install -U
+
+    echo "Do you want to save a new requirements.txt in $PWD?"
+    select yn in 'Yes' 'No'; do
+        case $yn in
+            Yes)
+                pip freeze --local > "$PWD/requirements.txt"; break;;
+            No)
+                exit;;
+        esac
+    done
+else
+    echo 'It is meant to run inside a virtualenv!'
+fi
