9455859a95f6c53922a9fe1fe3e3e48fbd8815d2
Fellipe Castro <contact@fellipecastro.com>
2014-06-15 10:41:59 -0300

    Update repository

--- pip-upgrader.sh
+++ pip-upgrader.sh
@@ -1,1 +1,11 @@
 pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip install -U
+
+echo 'Do you want to save a new requirements.txt?'
+select yn in 'Yes' 'No'; do
+    case $yn in
+        Yes)
+            pip freeze --local > "$PWD/requirements.txt"; break;;
+        No)
+            exit;;
+    esac
+done
